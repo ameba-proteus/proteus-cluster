a7f3d265b30de4b2675e83e7682894579cd5074c
matsukaz <matsukaz@gmail.com>
2012-11-28 15:19:58 +0900

    added maxForkCount in case of worker forked too many times.

--- test/cluster.js
+++ test/cluster.js
@@ -5,11 +5,12 @@ conf.worker = 2;
 conf.pid = '/tmp/proteus-cluster.pid';
 conf.exec = __dirname + '/worker.js';
 conf.disconnectTimeout = 120000;
+conf.maxForkCount = 100;
 
 var cnt = 0;
 cluster.addMessageListener('fromWorker', function(msg) {
 	cnt++;
-	if (cnt === 4) {
+	if (cnt > 10) {
 		cluster.shutdown();
 	}
 });
