2792d97ec85ce87f033bc710c1a55113da73dc84
Noritaka Okada <okada_noritaka@cyberagent.co.jp>
2015-08-17 11:06:59 +0900

    fix test

--- test/worker.js
+++ test/worker.js
@@ -1,40 +1,42 @@
+'use strict';
 var loggers = require('proteus-logger');
 loggers.configure({
-	appenders: {
-		console: {
-			type: 'console'
-		},
-		file: {
-			type: 'rotate_file',
-			layout: {
-				pattern: "%yyyy-%MM-%dd%T%HH:%mm:%ss %level %logger %msg %args (%line)%nstack%n"
-			}
-		}
-	},
-	loggers: {
-		"default": {
-			appenders: ["file","console"],
-			level: "debug"
-		}
-	}
+  appenders: {
+    console: {
+      type: 'console'
+    },
+    file: {
+      type: 'rotate_file',
+      layout: {
+        pattern: "%yyyy-%MM-%dd%T%HH:%mm:%ss %level %logger %msg %args (%line)%nstack%n"
+      }
+    }
+  },
+  loggers: {
+    "default": {
+      appenders: ["file","console"],
+      level: "debug"
+    }
+  }
 });
 var logger = loggers.get('cluster-test');
 
-logger.debug(process.argv);
+//logger.debug(process.argv);
 
-function sendMessage() {
-	try {
-		// logs will be sent to the master process
-//		logger.debug('[worker] going to send message from worker');
+function sendPong() {
+  try {
+    logger.debug('[worker] going to send message from worker');
 
-		process.send({cmd: 'fromWorker', msg: 'message from worker ' + process.pid});
-		process.exit(0);
-	} catch(e){
-		process.exit(0);
-	}
+    process.send({cmd: 'pong', msg: 'message from worker ' + process.pid});
+    process.exit(0);
+  } catch(e){
+    process.exit(0);
+  }
 }
 
-process.on('message', function(msg) {
-//	logger.debug('[worker] message received : ' + JSON.stringify(msg));
-	sendMessage();
+process.on('message', function(obj) {
+  logger.debug('[worker] message received : ' + JSON.stringify(obj));
+  if (obj.cmd === 'ping') {
+    sendPong();
+  }
 });
